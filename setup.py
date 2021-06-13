531612b9dc41539302d616bed0c5378fd2e43178
johnbumgarner <50935186+johnbumgarner@users.noreply.github.com>
2021-06-12 23:06:50 -0400

    Updated 06-12-2021

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wordhoard",
-    version="1.2",
+    version="1.4.5",
     author="John Bumgarner",
     author_email="wordhoardproject@gmail.com",
     description="lexical processing for textual analysis",
@@ -13,8 +13,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/johnbumgarner/wordhoard",
     packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={'files': ['files/common_english_homophones.pkl',
+                            'files/no_homophones_english.pkl']},
     license='LICENSE.txt',
     classifiers=["License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
+    keywords='antonyms, synonyms, hypernyms, hyponyms, homophones, definitions, lexicon, wordsearch',
     python_requires='>=3.6',
     install_requires=['bs4', 'lxml', 'requests', 'urllib3']
 )
+
