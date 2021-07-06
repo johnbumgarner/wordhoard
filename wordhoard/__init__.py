0f924e3dfa964f6685b716d69d752fae075ea873
johnbumgarner <50935186+johnbumgarner@users.noreply.github.com>
2021-07-06 08:18:09 -0400

    __init__.py updated 07-06-2021

--- wordhoard/__init__.py
+++ wordhoard/__init__.py
@@ -1,1 +1,18 @@
+__author__ = 'John Bumgarner'
+__date__ = 'July 5, 2021'
+__status__ = 'Production'
+__license__ = 'MIT'
+__copyright__ = "Copyright (C) 2021 John Bumgarner"
 
+from .antonyms import Antonyms
+from .synonyms import Synonyms
+from .hyponyms import Hyponyms
+from .hypernyms import Hypernyms
+from .homophones import Homophones
+from .dictionary import Definitions
+
+import logging
+from utilities import wordhoard_logger
+
+logger = logging.getLogger(__name__)
+wordhoard_logger.enable_logging(logger)
