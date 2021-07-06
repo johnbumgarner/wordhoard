9984cae71ffef67021b5eab97574aa06b9a2df6f
johnbumgarner <50935186+johnbumgarner@users.noreply.github.com>
2021-07-06 08:22:51 -0400

    Updated 07-06-2021

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("PYPI_description.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -8,7 +8,8 @@ setuptools.setup(
     version="1.4.5",
     author="John Bumgarner",
     author_email="wordhoardproject@gmail.com",
-    description="lexical processing for textual analysis",
+    description="A comprehensive lexical discovery application that is useful for finding semantic relationships between "
+                "words including a word's synonyms, synonyms, hypernyms, hyponyms and homophones.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/johnbumgarner/wordhoard",
@@ -17,9 +18,25 @@ setuptools.setup(
     package_data={'files': ['files/common_english_homophones.pkl',
                             'files/no_homophones_english.pkl']},
     license='LICENSE.txt',
-    classifiers=["License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
-    keywords='antonyms, synonyms, hypernyms, hyponyms, homophones, definitions, lexicon, wordsearch',
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: MIT License",
+                 "Natural Language :: English",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.6",
+                 "Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9"
+                 "Topic :: Software Development :: Libraries"
+                 "Topic :: Text Processing :: Linguistic",
+                 "Topic :: Utilities"],
+    keywords=['antonyms', 'synonyms', 'hypernyms', 'hyponyms', 'homophones', 'definitions', 'lexicon',
+              'semantic relationships', 'natural language processing'],
     python_requires='>=3.6',
-    install_requires=['bs4', 'lxml', 'requests', 'urllib3']
+    install_requires=['bs4',
+                      'deep-translator',
+                      'lxml',
+                      'requests',
+                      'urllib3']
 )
 
